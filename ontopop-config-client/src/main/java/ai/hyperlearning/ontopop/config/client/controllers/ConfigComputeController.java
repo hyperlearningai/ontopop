a4b30e303037fb71fd4ccddc5ec679fd1928235e
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-01 14:48:19 +0000

    Make controllers placeholders

--- ontopop-config-client/src/main/java/ai/hyperlearning/ontopop/config/client/controllers/ConfigComputeController.java
+++ ontopop-config-client/src/main/java/ai/hyperlearning/ontopop/config/client/controllers/ConfigComputeController.java
@@ -1,12 +1,8 @@
 package ai.hyperlearning.ontopop.config.client.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
@@ -21,22 +17,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "config", description = "Configuration API - Compute Services")
 public class ConfigComputeController {
 	
-	@Value("${ontopop.services.compute.ingestors.ontology.storage.outputDirectory}")
-    private String ingestorOntologyStorageOutputDirectory;
 	
-	/**************************************************************************
-	 * HTTP GET REQUESTS
-	 *************************************************************************/
-	
-	@Operation(
-			summary = "Ingestor - Ontology Storage Output Directory",
-			description = "Get the ontology ingestion service storage output directory path",
-			tags = { "service", "compute", "ontology", "ingest" })
-	@GetMapping(
-			value = "/ingestors/ontology/storage/outputDirectory", 
-			produces = MediaType.TEXT_PLAIN_VALUE)
-	public String getIngestorOntologyStorageOutputDirectory() {
-		return ingestorOntologyStorageOutputDirectory;
-	}
 
 }
