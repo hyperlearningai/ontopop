15c2991b35f8c19ac529b519c648a878f8eebcd2
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-03 16:40:23 +0000

    Add POST mapping

--- ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyController.java
+++ ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyController.java
@@ -2,9 +2,18 @@ package ai.hyperlearning.ontopop.data.ontology;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import ai.hyperlearning.ontopop.model.ontology.Ontology;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
@@ -22,15 +31,40 @@ public class OntologyController {
 	private static final Logger LOGGER = 
 			LoggerFactory.getLogger(OntologyController.class);
 	
+	@Autowired
+	private OntologyService ontologyService;
+	
 	/**************************************************************************
 	 * 1. POST - Create Ontology
 	 *************************************************************************/
 	
+	@Operation(
+			summary = "Create an ontology",
+			description = "Create an ontology",
+			tags = { "ontology" })
+	@ApiResponses(value = {
+	        @ApiResponse(
+	        		responseCode = "201",
+	        		description = "Ontology created"), 
+	        @ApiResponse(
+	        		responseCode = "401",
+	        		description = "Ontology creation unauthorized")})
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(produces = { "application/json" })
+	public Ontology newOntology(@RequestBody Ontology newOntology) {
+		return ontologyService.create(newOntology);
+	}
+	
+	/**************************************************************************
+	 * 2.1. GET - Get Ontologies
+	 *************************************************************************/
+	
 	
 	
 	/**************************************************************************
-	 * 2. GET - Get Ontology
+	 * 2.2. GET - Get Ontology
 	 *************************************************************************/
+	
 	
 	
 	
--- ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyService.java
+++ ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyService.java
@@ -2,7 +2,15 @@ package ai.hyperlearning.ontopop.data.ontology;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.vault.core.VaultTemplate;
+
+import ai.hyperlearning.ontopop.data.jpa.repositories.OntologyRepository;
+import ai.hyperlearning.ontopop.model.ontology.Ontology;
+import ai.hyperlearning.ontopop.model.ontology.OntologySecretData;
+import ai.hyperlearning.ontopop.security.vault.Vault;
 
 /**
  * Ontology Service
@@ -16,5 +24,48 @@ public class OntologyService {
 	
 	private static final Logger LOGGER = 
 			LoggerFactory.getLogger(OntologyService.class);
+	private static final String VAULT_SUBPATH_ONTOLOGIES = "/ontologies/";
+	
+	@Autowired
+    private OntologyRepository ontologyRepository;
+	
+	@Autowired
+	private VaultTemplate vaultTemplate;
+	
+	@Value("${spring.cloud.vault.kv.backend}")
+	String springCloudVaultKvBackend;
+	
+	@Value("${spring.cloud.vault.kv.default-context}")
+	String springCloudVaultKvDefaultContext;
+	
+	/**
+	 * Create a new ontology
+	 * @param ontology
+	 * @return
+	 */
+	
+	protected Ontology create(Ontology ontology) {
+		
+		// Persist the new ontology
+		Ontology newOntology = ontologyRepository.save(ontology);
+		
+		// Create a new ontology secret data object
+		OntologySecretData newOntologySecretData = 
+				new OntologySecretData(newOntology);
+		
+		// Persist the new ontology secret data
+		Vault.put(vaultTemplate, 
+				springCloudVaultKvBackend, 
+				springCloudVaultKvDefaultContext 
+					+ VAULT_SUBPATH_ONTOLOGIES 
+						+ newOntology.getId(), 
+				newOntologySecretData);
+		
+		// Sanitize and return the new ontology
+		newOntology.clearSecretData();
+		LOGGER.debug("Created a new ontology: {}", newOntology.toString());
+		return newOntology;
+		
+	}
 
 }
