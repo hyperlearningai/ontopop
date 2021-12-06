500f9985b665ac8c7c9f4e9fdcd3396b7ba66be1
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-06 13:42:06 +0000

    Add Repository annotation

--- ontopop-data-jpa/src/main/java/ai/hyperlearning/ontopop/data/jpa/repositories/OntologyRepository.java
+++ ontopop-data-jpa/src/main/java/ai/hyperlearning/ontopop/data/jpa/repositories/OntologyRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import ai.hyperlearning.ontopop.model.ontology.Ontology;
 
@@ -14,6 +15,7 @@ import ai.hyperlearning.ontopop.model.ontology.Ontology;
  * @since 2.0.0
  */
 
+@Repository
 public interface OntologyRepository extends CrudRepository<Ontology, Integer> {
 	
 	@Query("SELECT o FROM Ontology o WHERE o.repoUrl = ?1 AND o.repoOwner = ?2 AND o.repoResourcePath = ?3 AND o.repoBranch = ?4")
