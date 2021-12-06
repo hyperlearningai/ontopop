4ab3e4e809cd9a3a5ddbd6201d451e82ec2cc20a
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-06 13:42:27 +0000

    Add EnableJpaRepositories annotation

--- ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyApp.java
+++ ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyApp.java
@@ -2,6 +2,9 @@ package ai.hyperlearning.ontopop.data.ontology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Ontology Service - Spring Boot Application
@@ -10,6 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2.0.0
  */
 
+@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
+@EnableJpaRepositories("ai.hyperlearning.ontopop.data.jpa.repositories")
+@EntityScan("ai.hyperlearning.ontopop.model")
 @SpringBootApplication
 public class OntologyApp {
 	
