ecd1121af210d307bfcd792361c991a0cc6edd49
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-06 15:35:24 +0000

    Make ontology service transactional

--- ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyApp.java
+++ ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Ontology Service - Spring Boot Application
@@ -14,7 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 
 @ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
-@EnableJpaRepositories("ai.hyperlearning.ontopop.data.jpa.repositories")
 @EntityScan("ai.hyperlearning.ontopop.model")
 @SpringBootApplication
 public class OntologyApp {
--- ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyService.java
+++ ontopop-data-ontology/src/main/java/ai/hyperlearning/ontopop/data/ontology/OntologyService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.core.VaultKeyValueOperationsSupport.KeyValueBackend;
 
@@ -28,6 +29,7 @@ import ai.hyperlearning.ontopop.security.vault.Vault;
  */
 
 @Service
+@Transactional
 public class OntologyService {
 	
 	private static final Logger LOGGER = 
