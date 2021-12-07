493ea93086b668872b58389aa4c3beb9fa5f1de4
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-07 14:54:06 +0000

    Add find by ontology ID queries

--- ontopop-data-jpa/src/main/java/ai/hyperlearning/ontopop/data/jpa/repositories/WebhookEventRepository.java
+++ ontopop-data-jpa/src/main/java/ai/hyperlearning/ontopop/data/jpa/repositories/WebhookEventRepository.java
@@ -1,6 +1,11 @@
 package ai.hyperlearning.ontopop.data.jpa.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import ai.hyperlearning.ontopop.model.git.WebhookEvent;
 
@@ -11,6 +16,14 @@ import ai.hyperlearning.ontopop.model.git.WebhookEvent;
  * @since 2.0.0
  */
 
+@Repository
 public interface WebhookEventRepository extends CrudRepository<WebhookEvent, Long> {
+	
+	@Query("SELECT w FROM WebhookEvent w WHERE w.ontology.id = ?1")
+	List<WebhookEvent> findByOntologyId(int ontologyId);
+	
+	@Query("SELECT w FROM WebhookEvent w WHERE w.ontology.id = ?1 AND w.id = ?2")
+	Optional<WebhookEvent> findByOntologyIdAdnWebhookEventId(
+			int ontologyId, long webhookEventId);
 
 }
