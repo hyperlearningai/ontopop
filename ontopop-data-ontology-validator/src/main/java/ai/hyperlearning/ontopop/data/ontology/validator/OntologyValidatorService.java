f82cf087ad784aa749d9ace4b2f6369626b11189
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-15 12:32:34 +0000

    Improve code readability

--- ontopop-data-ontology-validator/src/main/java/ai/hyperlearning/ontopop/data/ontology/validator/OntologyValidatorService.java
+++ ontopop-data-ontology-validator/src/main/java/ai/hyperlearning/ontopop/data/ontology/validator/OntologyValidatorService.java
@@ -167,6 +167,7 @@ public class OntologyValidatorService {
 				downloadedFileUri);
 		LOGGER.info("Ontology Validation Service - "
 				+ "Finished downloading the ingested resource.");
+		
 	}
 	
 	/**
