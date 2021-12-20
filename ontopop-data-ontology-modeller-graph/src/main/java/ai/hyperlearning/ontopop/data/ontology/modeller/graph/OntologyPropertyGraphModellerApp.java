cd39ee67041550dd68f285c8f4787d2f442449a0
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-20 10:12:14 +0000

    Add modeller app and service

---
+++ ontopop-data-ontology-modeller-graph/src/main/java/ai/hyperlearning/ontopop/data/ontology/modeller/graph/OntologyPropertyGraphModellerApp.java
@@ -0,0 +1,72 @@
+package ai.hyperlearning.ontopop.data.ontology.modeller.graph;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ai.hyperlearning.ontopop.messaging.processors.DataPipelineModellerSource;
+import ai.hyperlearning.ontopop.model.ontology.OntologyMessage;
+
+/**
+ * Ontology Property Graph Modelling Service - Spring Boot Application
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@SuppressWarnings("deprecation")
+@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
+@SpringBootApplication
+@EnableBinding(DataPipelineModellerSource.class)
+public class OntologyPropertyGraphModellerApp {
+	
+	private static final Logger LOGGER = 
+			LoggerFactory.getLogger(OntologyPropertyGraphModellerApp.class);
+	
+	@Autowired
+	private OntologyPropertyGraphModellingService ontologyPropertyGraphModellingService;
+	
+	public static void main(String[] args) {
+        SpringApplication.run(OntologyPropertyGraphModellerApp.class, args);
+	}
+	
+	@StreamListener("parsedConsumptionChannel")
+	public void processParsedOntology(String payload) {
+		
+		try {
+			
+			// Explicitly check that the string payload 
+			// models an OntologyMessage object
+			ObjectMapper mapper = new ObjectMapper();
+			OntologyMessage ontologyMessage = 
+					mapper.readValue(payload, OntologyMessage.class);
+			
+			// Log the consumed payload for debugging purposes
+			LOGGER.debug("New ontology parsed event detected and consumed "
+					+ "via the shared messaging service and the "
+					+ "parsedConsumptionChannel channel.");
+			LOGGER.debug("Ontology parsed message payload: {}", payload);
+			
+			// Run the Ontology Property Graph modelling service pipeline
+			ontologyPropertyGraphModellingService.run(ontologyMessage);
+			
+		} catch (JsonProcessingException e) {
+			
+			LOGGER.info("New parsed event detected and consumed via "
+					+ "the shared messaging service and the "
+					+ "parsedConsumptionChannel channel.");
+			LOGGER.info("The parsed object is NOT an ontology. Skipping.");
+			
+		}
+		
+	}
+			
+}
---
+++ ontopop-data-ontology-modeller-graph/src/main/java/ai/hyperlearning/ontopop/data/ontology/modeller/graph/OntologyPropertyGraphModellingService.java
@@ -0,0 +1,260 @@
+package ai.hyperlearning.ontopop.data.ontology.modeller.graph;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import ai.hyperlearning.ontopop.messaging.processors.DataPipelineModellerSource;
+import ai.hyperlearning.ontopop.model.graph.SimpleOntologyPropertyGraph;
+import ai.hyperlearning.ontopop.model.ontology.OntologyMessage;
+import ai.hyperlearning.ontopop.model.owl.SimpleOntology;
+import ai.hyperlearning.ontopop.storage.ObjectStorageService;
+import ai.hyperlearning.ontopop.storage.ObjectStorageServiceFactory;
+import ai.hyperlearning.ontopop.storage.ObjectStorageServiceType;
+
+/**
+ * Ontology Property Graph Modelling Service
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@SuppressWarnings("deprecation")
+@Service
+@EnableBinding(DataPipelineModellerSource.class)
+public class OntologyPropertyGraphModellingService {
+	
+	private static final Logger LOGGER = 
+			LoggerFactory.getLogger(OntologyPropertyGraphModellingService.class);
+	
+	@Autowired
+	private ObjectStorageServiceFactory objectStorageServiceFactory;
+	
+	@Autowired
+	private DataPipelineModellerSource dataPipelineModellerSource;
+	
+	@Value("${storage.object.service}")
+	private String storageObjectService;
+	
+	@Value("${storage.object.local.baseUri}")
+	private String storageLocalBaseUri;
+	
+	@Value("${storage.object.containers.parsed}")
+	private String parsedDirectoryName;
+	
+	@Value("${storage.object.containers.modelled}")
+	private String modelledDirectoryName;
+	
+	@Value("${storage.object.patterns.fileNameIdsSeparator}")
+	private String filenameIdsSeparator;
+	
+	private OntologyMessage ontologyMessage;
+	private ObjectStorageService objectStorageService;
+	private String readObjectUri;
+	private String writeDirectoryUri;
+	private String downloadedFileUri;
+	private SimpleOntology simpleOntology;
+	private SimpleOntologyPropertyGraph simpleOntologyPropertyGraph;
+	
+	/**
+	 * Run the Ontology Parsing service end-to-end pipeline
+	 */
+	
+	public void run(OntologyMessage ontologyMessage) {
+		
+		LOGGER.info("Ontology Property Graph Modelling Service started.");
+		this.ontologyMessage = ontologyMessage;
+		
+		try {
+			
+			// 1. Environment setup
+			setup();
+			
+			// 2. Download the parsed ontology from persistent storage
+			download();
+			
+			// 3. Model the ontology as a directed property graph
+			model();
+			
+			// 4. Persist the modelled ontology
+			persist();
+			
+			// 5. Publish a message to the shared messaging system
+			publish();
+			
+			// 6. Cleanup resources
+			cleanup();
+			
+		} catch (Exception e) {
+			LOGGER.error("Ontology Property Graph Modelling Service "
+					+ "encountered an error.", e);
+		}
+		
+	}
+	
+	/**
+	 * Instantiate the relevant object storage service
+	 * @throws IOException
+	 */
+	
+	private void setup() throws IOException {
+		
+		// 1. Select the relevant persistent storage service and 
+		// create the target directory if it does not already exist
+		ObjectStorageServiceType objectStorageServiceType = 
+				ObjectStorageServiceType.valueOfLabel(
+						storageObjectService.toUpperCase());
+		objectStorageService = objectStorageServiceFactory
+				.getObjectStorageService(objectStorageServiceType);
+		LOGGER.debug("Using the {} object storage service.", 
+				objectStorageServiceType);
+		
+		// 2. Define and create (if required) the relevant 
+		// target parsed directory
+		switch ( objectStorageServiceType ) {
+			
+			case LOCAL:
+				
+				// Create (if required) the local target modelled directory
+				readObjectUri = storageLocalBaseUri 
+					+ File.separator + parsedDirectoryName 
+					+ File.separator + ontologyMessage.getJsonProcessedFilename();
+				writeDirectoryUri = storageLocalBaseUri + 
+					File.separator + modelledDirectoryName;
+				if ( !objectStorageService.doesContainerExist(writeDirectoryUri) )
+					objectStorageService.createContainer(writeDirectoryUri);
+				break;
+				
+			default:
+				
+				// Create (if required) the Azure Storage container 
+				// or AWS S3 bucket
+				readObjectUri = parsedDirectoryName + "/" 
+						+ ontologyMessage.getJsonProcessedFilename();
+				writeDirectoryUri = modelledDirectoryName;
+				if ( !objectStorageService.doesContainerExist(writeDirectoryUri) )
+					objectStorageService.createContainer(writeDirectoryUri);
+			
+		}
+		
+	}
+	
+	/**
+	 * Download the parsed ontology from persistent storage
+	 * to a temporary file in local storage
+	 * @throws IOException
+	 */
+	
+	private void download() throws IOException {
+		
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Started downloading the parsed resource.");
+		downloadedFileUri = objectStorageService.downloadObject(readObjectUri, 
+				filenameIdsSeparator + ontologyMessage.getJsonProcessedFilename());
+		LOGGER.debug("Downloaded parsed resource to '{}'.", 
+				downloadedFileUri);
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Finished downloading the parsed resource.");
+		
+	}
+	
+	/**
+	 * Model the ontology as a directed property graph
+	 * @throws IOException
+	 */
+	
+	private void model() throws IOException {
+		
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Started modelled the validated resource.");
+		
+		// Deserialize the Simple Ontology object from JSON
+		ObjectMapper mapper = new ObjectMapper();
+		simpleOntology = mapper.readValue(
+				new File(downloadedFileUri), SimpleOntology.class);
+		
+		// Transform the Simple Ontology object into a 
+		// Simple Ontology Property Graph object
+		simpleOntologyPropertyGraph = new SimpleOntologyPropertyGraph(
+				ontologyMessage.getOntologyId(), 
+				ontologyMessage.getWebhookEventId(), 
+				simpleOntology);
+		
+		LOGGER.debug("Modelled ontology: '{}'.", simpleOntologyPropertyGraph);
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Finished modelled the validated resource.");
+		
+	}
+	
+	/**
+	 * Persist the modelled ontology
+	 * @throws IOException
+	 */
+	
+	private void persist() throws IOException {
+		
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Started the persistence of the modelled resource.");
+		
+		// Serialize the Simple Ontology Property Graph object 
+		// to a temporary file in the local file system
+		String jsonFilename = ontologyMessage.getJsonProcessedFilename();
+		Path temporaryFile = Files.createTempFile("", jsonFilename);
+		File file = new File(temporaryFile.toAbsolutePath().toString());
+		ObjectMapper mapper = new ObjectMapper()
+				.enable(SerializationFeature.INDENT_OUTPUT);;
+		mapper.writeValue(file, simpleOntologyPropertyGraph);
+		
+		// Upload the serialized JSON file to persistent object storage
+		String targetFilepath = writeDirectoryUri + "/" + 
+				ontologyMessage.getJsonProcessedFilename();
+		objectStorageService.uploadObject(
+				temporaryFile.toAbsolutePath().toString(), 
+				targetFilepath);
+		
+		LOGGER.debug("Successfully persisted modelled ontology "
+				+ "resource to '{}'.", targetFilepath);
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Finished the persistence of the modelled resource.");
+		
+	}
+	
+	/**
+	 * Publish a message to the shared messaging system
+	 */
+	
+	private void publish() {
+		
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Started publishing message.");
+		dataPipelineModellerSource.modelledPublicationChannel()
+			.send(MessageBuilder.withPayload(ontologyMessage).build());
+		LOGGER.info("Ontology Property Graph Modelling Service - "
+				+ "Finished publishing message.");
+		
+	}
+	
+	/**
+	 * Cleanup any open resources
+	 */
+	
+	private void cleanup() throws IOException {
+		
+		// Close any storage service clients
+		objectStorageService.cleanup();
+		
+	}
+
+}
