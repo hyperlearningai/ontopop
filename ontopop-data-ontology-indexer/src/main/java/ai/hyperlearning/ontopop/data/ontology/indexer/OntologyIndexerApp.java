a7893b53a72d945135b77ee857f30a4816c8c77c
jillurquddus <jillur.quddus@hyperlearning.ai>
2021-12-29 10:06:28 +0000

    Add initial ontology indexing service

---
+++ ontopop-data-ontology-indexer/src/main/java/ai/hyperlearning/ontopop/data/ontology/indexer/OntologyIndexerApp.java
@@ -0,0 +1,72 @@
+package ai.hyperlearning.ontopop.data.ontology.indexer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ai.hyperlearning.ontopop.messaging.processors.DataPipelineModelledIndexerSource;
+import ai.hyperlearning.ontopop.model.ontology.OntologyMessage;
+
+/**
+ * Ontology Indexer Service - Spring Boot Application
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@SuppressWarnings("deprecation")
+@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
+@SpringBootApplication
+@EnableBinding(DataPipelineModelledIndexerSource.class)
+public class OntologyIndexerApp {
+	
+	private static final Logger LOGGER = 
+			LoggerFactory.getLogger(OntologyIndexerApp.class);
+	
+	@Autowired
+	private OntologyIndexerService ontologyIndexerService;
+	
+	public static void main(String[] args) {
+        SpringApplication.run(OntologyIndexerApp.class, args);
+	}
+	
+	@StreamListener("modelledConsumptionChannel")
+	public void processModelledOntology(String payload) {
+		
+		try {
+			
+			// Explicitly check that the string payload 
+			// models an OntologyMessage object
+			ObjectMapper mapper = new ObjectMapper();
+			OntologyMessage ontologyMessage = 
+					mapper.readValue(payload, OntologyMessage.class);
+			
+			// Log the consumed payload for debugging purposes
+			LOGGER.debug("New ontology modelled event detected and consumed "
+					+ "via the shared messaging service and the "
+					+ "modelledConsumptionChannel channel.");
+			LOGGER.debug("Ontology modelled message payload: {}", payload);
+			
+			// Run the Ontology Indexing Service pipeline
+			ontologyIndexerService.run(ontologyMessage);
+			
+		} catch (JsonProcessingException e) {
+			
+			LOGGER.info("New modelled event detected and consumed via "
+					+ "the shared messaging service and the "
+					+ "modelledConsumptionChannel channel.");
+			LOGGER.info("The validated object is NOT an ontology. Skipping.");
+			
+		}
+		
+	}
+
+}
---
+++ ontopop-data-ontology-indexer/src/main/java/ai/hyperlearning/ontopop/data/ontology/indexer/OntologyIndexerService.java
@@ -0,0 +1,290 @@
+package ai.hyperlearning.ontopop.data.ontology.indexer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ai.hyperlearning.ontopop.messaging.processors.DataPipelineModelledIndexerSource;
+import ai.hyperlearning.ontopop.model.graph.SimpleOntologyPropertyGraph;
+import ai.hyperlearning.ontopop.model.graph.SimpleOntologyVertex;
+import ai.hyperlearning.ontopop.model.ontology.OntologyMessage;
+import ai.hyperlearning.ontopop.search.SearchService;
+import ai.hyperlearning.ontopop.search.SearchServiceFactory;
+import ai.hyperlearning.ontopop.search.SearchServiceType;
+import ai.hyperlearning.ontopop.search.model.SimpleIndexVertex;
+import ai.hyperlearning.ontopop.storage.ObjectStorageService;
+import ai.hyperlearning.ontopop.storage.ObjectStorageServiceFactory;
+import ai.hyperlearning.ontopop.storage.ObjectStorageServiceType;
+
+/**
+ * Ontology Indexing Service
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@SuppressWarnings("deprecation")
+@Service
+@EnableBinding(DataPipelineModelledIndexerSource.class)
+public class OntologyIndexerService {
+	
+	private static final Logger LOGGER = 
+			LoggerFactory.getLogger(OntologyIndexerService.class);
+	
+	@Autowired
+	private ObjectStorageServiceFactory objectStorageServiceFactory;
+	
+	@Autowired
+	private SearchServiceFactory searchServiceFactory;
+	
+	@Autowired
+	private DataPipelineModelledIndexerSource dataPipelineModelledIndexerSource;
+	
+	@Value("${storage.object.service}")
+	private String storageObjectService;
+	
+	@Value("${storage.object.local.baseUri}")
+	private String storageLocalBaseUri;
+	
+	@Value("${storage.object.containers.modelled}")
+	private String modelledDirectoryName;
+	
+	@Value("${storage.object.containers.indexed}")
+	private String indexedDirectoryName;
+	
+	@Value("${storage.object.patterns.fileNameIdsSeparator}")
+	private String filenameIdsSeparator;
+	
+	@Value("${storage.search.service}")
+	private String storageSearchService;
+	
+	@Value("${storage.search.indexNamePrefix}")
+	private String searchIndexNamePrefix;
+	
+	private OntologyMessage ontologyMessage;
+	private ObjectStorageService objectStorageService;
+	private SearchService searchService;
+	private String readObjectUri;
+	private String writeDirectoryUri;
+	private String downloadedFileUri;
+	
+	/**
+	 * Run the Ontology Indexing service end-to-end pipeline
+	 */
+	
+	public void run(OntologyMessage ontologyMessage) {
+		
+		LOGGER.info("Ontology Indexing Service started.");
+		this.ontologyMessage = ontologyMessage;
+		
+		try {
+			
+			// 1. Environment setup
+			setup();
+			
+			// 2. Download the modelled ontology from persistent storage
+			download();
+			
+			// 3. Index the ontology into a search index
+			index();
+			
+			// 4. Copy the modelled ontology to the indexed directory 
+			// in persistent storage 
+			copy();
+			
+			// 5. Publish a message to the shared messaging system
+			publish();
+			
+			// 6. Cleanup resources
+			cleanup();
+			
+		} catch (Exception e) {
+			LOGGER.error("Ontology Indexing Service "
+					+ "encountered an error.", e);
+		}
+		
+		LOGGER.info("Ontology Indexing Service finished.");
+		
+	}
+	
+	/**
+	 * Instantiate the relevant object storage service
+	 * @throws IOException
+	 */
+	
+	private void setup() throws IOException {
+		
+		// 1. Select the relevant persistent storage service and 
+		// create the target directory if it does not already exist
+		ObjectStorageServiceType objectStorageServiceType = 
+				ObjectStorageServiceType.valueOfLabel(
+						storageObjectService.toUpperCase());
+		objectStorageService = objectStorageServiceFactory
+				.getObjectStorageService(objectStorageServiceType);
+		LOGGER.debug("Using the {} object storage service.", 
+				objectStorageServiceType);
+		
+		// 2. Define and create (if required) the relevant 
+		// target indexed directory
+		switch ( objectStorageServiceType ) {
+			
+			case LOCAL:
+				
+				// Create (if required) the local target indexed directory
+				readObjectUri = storageLocalBaseUri 
+					+ File.separator + modelledDirectoryName 
+					+ File.separator + ontologyMessage.getJsonProcessedFilename();
+				writeDirectoryUri = storageLocalBaseUri + 
+					File.separator + indexedDirectoryName;
+				if ( !objectStorageService.doesContainerExist(writeDirectoryUri) )
+					objectStorageService.createContainer(writeDirectoryUri);
+				break;
+				
+			default:
+				
+				// Create (if required) the Azure Storage container 
+				// or AWS S3 bucket
+				readObjectUri = modelledDirectoryName + "/" 
+						+ ontologyMessage.getJsonProcessedFilename();
+				writeDirectoryUri = indexedDirectoryName;
+				if ( !objectStorageService.doesContainerExist(writeDirectoryUri) )
+					objectStorageService.createContainer(writeDirectoryUri);
+			
+		}
+		
+		// 3. Select the relevant graph database service
+		SearchServiceType searchServiceType = SearchServiceType.valueOfLabel(
+						storageSearchService.toUpperCase());
+		searchService = searchServiceFactory
+				.getSearchService(searchServiceType);
+		LOGGER.debug("Using the {} search service.", 
+				searchServiceType);
+		
+	}
+	
+	/**
+	 * Download the modelled ontology from persistent storage
+	 * to a temporary file in local storage
+	 * @throws IOException
+	 */
+	
+	private void download() throws IOException {
+		
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Started downloading the modelled resource.");
+		downloadedFileUri = objectStorageService.downloadObject(readObjectUri, 
+				filenameIdsSeparator + ontologyMessage.getJsonProcessedFilename());
+		LOGGER.debug("Downloaded modelled resource to '{}'.", 
+				downloadedFileUri);
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Finished downloading the modelled resource.");
+		
+	}
+	
+	/**
+	 * Index the modelled ontology into the relevant search index
+	 * @throws IOException
+	 */
+	
+	private void index() throws IOException {
+		
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Started indexing the modelled resource into "
+				+ "the search index.");
+		
+		// Deserialize the Simple Ontology Property Graph object from JSON
+		ObjectMapper mapper = new ObjectMapper();
+		SimpleOntologyPropertyGraph simpleOntologyPropertyGraph = 
+				mapper.readValue(new File(downloadedFileUri), 
+						SimpleOntologyPropertyGraph.class);
+		
+		// Delete the index for this ontology if it already exists
+		String indexName = searchIndexNamePrefix + 
+				ontologyMessage.getOntologyId();
+		searchService.deleteIndex(indexName);
+		
+		// Generate a set of SimpleIndexVertex objects
+		Set<SimpleIndexVertex> vertices = new LinkedHashSet<>();
+		for (var entry : simpleOntologyPropertyGraph.getVertices().entrySet()) {
+			SimpleOntologyVertex vertex = entry.getValue();
+			vertex.preparePropertiesForLoading();
+			vertices.add(new SimpleIndexVertex(
+					vertex.getVertexId(), 
+					SimpleOntologyVertex.LABEL, 
+					vertex.getProperties()));	
+		}
+		
+		// Bulk index the vertices/classes
+		searchService.indexDocuments(indexName, vertices);
+		LOGGER.debug("Indexed {} vertices.", vertices.size());
+		
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Finished loading the modelled resource into "
+				+ "the graph database.");
+		
+	}
+	
+	/**
+	 * Copy the modelled ontology to the indexed directory 
+	 * in persistent storage 
+	 * @throws IOException
+	 */
+	
+	private void copy() throws IOException {
+		
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Started the persistence of the loaded resource.");
+		String targetFilepath = writeDirectoryUri + "/" + 
+				ontologyMessage.getJsonProcessedFilename();
+		objectStorageService.uploadObject(
+				downloadedFileUri, 
+				targetFilepath);
+		LOGGER.debug("Successfully persisted loaded ontology "
+				+ "resource to '{}'.", targetFilepath);
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Finished the persistence of the loaded resource.");
+		
+	}
+	
+	/**
+	 * Publish a message to the shared messaging system
+	 */
+	
+	private void publish() {
+		
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Started publishing message.");
+		dataPipelineModelledIndexerSource.modelledIndexedPublicationChannel()
+			.send(MessageBuilder.withPayload(ontologyMessage).build());
+		LOGGER.info("Ontology Indexing Service - "
+				+ "Finished publishing message.");
+		
+	}
+	
+	/**
+	 * Cleanup any open resources
+	 * @throws Exception 
+	 */
+	
+	private void cleanup() throws Exception {
+		
+		// Close any storage service clients
+		objectStorageService.cleanup();
+		
+		// Close any search service clients
+		searchService.cleanup();
+		
+	}
+
+}
