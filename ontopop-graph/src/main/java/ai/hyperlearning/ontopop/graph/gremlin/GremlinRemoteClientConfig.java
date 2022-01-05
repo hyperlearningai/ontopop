99ce31afb95e24892130a093c36f1e8222f3f88e
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-05 17:08:48 +0000

    Update bean to create cluster from application.yml

--- ontopop-graph/src/main/java/ai/hyperlearning/ontopop/graph/gremlin/GremlinRemoteClientConfig.java
+++ ontopop-graph/src/main/java/ai/hyperlearning/ontopop/graph/gremlin/GremlinRemoteClientConfig.java
@@ -1,10 +1,14 @@
 package ai.hyperlearning.ontopop.graph.gremlin;
 
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.tinkerpop.gremlin.driver.Client;
 import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.ser.MessageTextSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,14 +23,54 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GremlinRemoteClientConfig {
 	
-	@Value("${storage.graph.gremlin-remote-graph.configuration-filename}")
-	private String gremlinRemoteConfigurationFilename;
+	@Value("${storage.graph.gremlin-remote-graph.host}")
+	private String host;
+	
+	@Value("${storage.graph.gremlin-remote-graph.port}")
+	private int port;
+	
+	@Value("${storage.graph.gremlin-remote-graph.username}")
+	private String username;
+	
+	@Value("${storage.graph.gremlin-remote-graph.password}")
+	private String password;
+	
+	@Value("${storage.graph.gremlin-remote-graph.enableSsl}")
+	private boolean enableSsl;
+	
+	@Value("${storage.graph.gremlin-remote-graph.serializer.className}")
+	private String serializerClassName;
+	
+	@Value("${storage.graph.gremlin-remote-graph.serializer.serializeResultToString}")
+	private boolean serializeResultToString;
 	
 	@Bean("gremlinRemoteGraphClient")
-	public Client getClient() throws FileNotFoundException {
-		Cluster cluster = Cluster.build(
-				new File(gremlinRemoteConfigurationFilename)).create();
+	public Client getClient() 
+			throws FileNotFoundException, ClassNotFoundException, 
+			NoSuchMethodException, SecurityException, InstantiationException, 
+			IllegalAccessException, IllegalArgumentException, 
+			InvocationTargetException {
+		
+		// Configure the serializer
+		final Map<String, Object> serializerSettings = new HashMap<>();
+		serializerSettings.put(
+				"serializeResultToString", serializeResultToString);
+		Class<?> clazz = Class.forName(serializerClassName);
+		Constructor<?> constructor = clazz.getConstructor();
+		MessageTextSerializer<?> serializer = 
+				(MessageTextSerializer<?>) constructor.newInstance();
+        serializer.configure(serializerSettings, null);
+		
+        // Create the connection to the remote Gremlin server
+		Cluster cluster = Cluster.build()
+				.addContactPoint(host)
+				.port(port)
+				.credentials(username, password)
+				.enableSsl(enableSsl)
+				.serializer(serializer)
+				.create();
 		return cluster.connect();
+		
 	}
 
 }
