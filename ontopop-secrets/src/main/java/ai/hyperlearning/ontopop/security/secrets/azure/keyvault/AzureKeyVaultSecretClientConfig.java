e6d9a7fd9b5109f42538cd9a8ca3f167a05392d7
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-12 12:43:46 +0000

    Read Azure Key Vault credentials from application.yml

--- ontopop-secrets/src/main/java/ai/hyperlearning/ontopop/security/secrets/azure/keyvault/AzureKeyVaultSecretClientConfig.java
+++ ontopop-secrets/src/main/java/ai/hyperlearning/ontopop/security/secrets/azure/keyvault/AzureKeyVaultSecretClientConfig.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 
@@ -24,11 +24,25 @@ public class AzureKeyVaultSecretClientConfig {
 
     @Value("${security.secrets.azure-key-vault.url}")
     private String vaultUrl;
+    
+    @Value("${security.secrets.azure-key-vault.clientId}")
+    private String clientId;
+    
+    @Value("${security.secrets.azure-key-vault.clientSecret}")
+    private String clientSecret;
+    
+    @Value("${security.secrets.azure-key-vault.tenantId}")
+    private String tenantId;
 
     @Bean
     public SecretClient getSecretClient() {
+        ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .tenantId(tenantId)
+                .build();
         return new SecretClientBuilder().vaultUrl(vaultUrl)
-                .credential(new DefaultAzureCredentialBuilder().build())
+                .credential(credential)
                 .buildClient();
     }
 
