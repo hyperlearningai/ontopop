90433f5b449bf8dfb60c776d66c0082f023776f3
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-14 14:12:38 +0000

    Execute ingestor function

--- ontopop-data-ontology-ingestor/src/main/java/ai/hyperlearning/ontopop/data/ontology/ingestor/OntologyIngestorController.java
+++ ontopop-data-ontology-ingestor/src/main/java/ai/hyperlearning/ontopop/data/ontology/ingestor/OntologyIngestorController.java
@@ -1,8 +1,7 @@
 package ai.hyperlearning.ontopop.data.ontology.ingestor;
 
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ai.hyperlearning.ontopop.data.ontology.ingestor.function.OntologyIngestorFunction;
+import ai.hyperlearning.ontopop.data.ontology.ingestor.function.OntologyIngestorFunctionModel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -29,11 +31,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "ingest", description = "Ontology Ingestion Service API")
 public class OntologyIngestorController {
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(OntologyIngestorController.class);
-
     @Autowired
-    private OntologyIngestorService ontologyIngestorService;
+    private OntologyIngestorFunction ontologyIngestorFunction;
 
     /**************************************************************************
      * 1. POST - Ontology Ingestion Webhook
@@ -56,17 +55,10 @@ public class OntologyIngestorController {
             @RequestHeader Map<String, String> headers,
             @RequestBody(required = true) String payload) {
 
-        // Log the HTTP request headers for debugging purposes
-        LOGGER.debug("New HTTP POST request: Ontology ingestion webhook.");
-        headers.forEach((key, value) -> {
-            LOGGER.debug(String.format("Header '%s' = %s", key, value));
-        });
-
-        // Log the HTTP request body payload for debugging purposes
-        LOGGER.debug("Ontology ingestion webhook payload: {}", payload);
-
-        // Run the Ontology Ingestion Service pipeline
-        ontologyIngestorService.run(headers, payload);
+        // Execute the Ontology Ingestor Function
+        OntologyIngestorFunctionModel ontologyIngestorFunctionModel = 
+                new OntologyIngestorFunctionModel(headers, payload);
+        ontologyIngestorFunction.apply(ontologyIngestorFunctionModel);
 
         // Return a response entity
         return new ResponseEntity<String>(
