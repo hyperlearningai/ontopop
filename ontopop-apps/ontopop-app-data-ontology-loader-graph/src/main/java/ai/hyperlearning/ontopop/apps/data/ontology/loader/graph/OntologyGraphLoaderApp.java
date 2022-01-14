e4ce5b61ebbac7fe2922eab4482ef971075907ff
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-14 14:00:55 +0000

    Separate application from service

---
+++ ontopop-apps/ontopop-app-data-ontology-loader-graph/src/main/java/ai/hyperlearning/ontopop/apps/data/ontology/loader/graph/OntologyGraphLoaderApp.java
@@ -0,0 +1,44 @@
+package ai.hyperlearning.ontopop.apps.data.ontology.loader.graph;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.ComponentScan;
+
+import ai.hyperlearning.ontopop.data.ontology.loader.graph.function.OntologyGraphLoaderFunction;
+import ai.hyperlearning.ontopop.data.ontology.loader.graph.function.OntologyGraphLoaderFunctionModel;
+import ai.hyperlearning.ontopop.messaging.processors.DataPipelineModelledLoaderSource;
+
+/**
+ * Ontology Graph Loading Service - Spring Boot Application
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@SuppressWarnings("deprecation")
+@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
+@SpringBootApplication
+@EnableBinding(DataPipelineModelledLoaderSource.class)
+public class OntologyGraphLoaderApp {
+    
+    @Autowired
+    private OntologyGraphLoaderFunction ontologyGraphLoaderFunction;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(OntologyGraphLoaderApp.class, args);
+    }
+    
+    @StreamListener("modelledConsumptionChannel")
+    public void processModelledOntology(String payload) {
+        
+        // Execute the Ontology Graph Loader Function
+        OntologyGraphLoaderFunctionModel ontologyGraphLoaderFunctionModel = 
+                new OntologyGraphLoaderFunctionModel(payload);
+        ontologyGraphLoaderFunction.apply(ontologyGraphLoaderFunctionModel);
+        
+    }
+
+}
--- ontopop-data-ontology-loader-graph/src/main/java/ai/hyperlearning/ontopop/data/ontology/loader/graph/function/OntologyGraphLoaderFunction.java
+++ ontopop-data-ontology-loader-graph/src/main/java/ai/hyperlearning/ontopop/data/ontology/loader/graph/function/OntologyGraphLoaderFunction.java
@@ -1,59 +1,55 @@
-package ai.hyperlearning.ontopop.data.ontology.loader.graph;
+package ai.hyperlearning.ontopop.data.ontology.loader.graph.function;
+
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import ai.hyperlearning.ontopop.messaging.processors.DataPipelineModelledLoaderSource;
+import ai.hyperlearning.ontopop.data.ontology.loader.graph.OntologyGraphLoaderService;
 import ai.hyperlearning.ontopop.model.ontology.OntologyMessage;
 
 /**
- * Ontology Graph Loading Service - Spring Boot Application
+ * Ontology Graph Loader Function
  *
  * @author jillurquddus
  * @since 2.0.0
  */
 
-@SuppressWarnings("deprecation")
-@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
-@SpringBootApplication
-@EnableBinding(DataPipelineModelledLoaderSource.class)
-public class OntologyGraphLoaderApp {
-
+@Component
+public class OntologyGraphLoaderFunction 
+    implements Function<OntologyGraphLoaderFunctionModel, Boolean> {
+    
     private static final Logger LOGGER =
-            LoggerFactory.getLogger(OntologyGraphLoaderApp.class);
-
+            LoggerFactory.getLogger(OntologyGraphLoaderFunction.class);
+    
     @Autowired
     private OntologyGraphLoaderService ontologyGraphLoaderService;
-
-    public static void main(String[] args) {
-        SpringApplication.run(OntologyGraphLoaderApp.class, args);
-    }
-
-    @StreamListener("modelledConsumptionChannel")
-    public void processModelledOntology(String payload) {
-
+    
+    @Override
+    public Boolean apply(
+            OntologyGraphLoaderFunctionModel ontologyGraphLoaderFunctionModel) {
+        
         try {
 
             // Explicitly check that the string payload
             // models an OntologyMessage object
             ObjectMapper mapper = new ObjectMapper();
             OntologyMessage ontologyMessage =
-                    mapper.readValue(payload, OntologyMessage.class);
+                    mapper.readValue(
+                            ontologyGraphLoaderFunctionModel.getPayload(), 
+                            OntologyMessage.class);
 
             // Log the consumed payload for debugging purposes
             LOGGER.debug("New ontology modelled event detected and consumed "
                     + "via the shared messaging service and the "
                     + "modelledConsumptionChannel channel.");
-            LOGGER.debug("Ontology modelled message payload: {}", payload);
+            LOGGER.debug("Ontology modelled message payload: {}", 
+                    ontologyGraphLoaderFunctionModel.getPayload());
 
             // Run the Ontology Graph Loading Service pipeline
             ontologyGraphLoaderService.run(ontologyMessage);
@@ -66,7 +62,9 @@ public class OntologyGraphLoaderApp {
             LOGGER.info("The validated object is NOT an ontology. Skipping.");
 
         }
-
+        
+        return true;
+        
     }
 
 }
