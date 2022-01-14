afe329db65e185331c6b56242d0b95c3c77d94b2
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-14 11:40:02 +0000

    Separate application from service

--- ontopop-data-ontology-parser/src/main/java/ai/hyperlearning/ontopop/data/ontology/parser/function/OntologyParserFunction.java
+++ ontopop-data-ontology-parser/src/main/java/ai/hyperlearning/ontopop/data/ontology/parser/function/OntologyParserFunction.java
@@ -1,59 +1,55 @@
-package ai.hyperlearning.ontopop.data.ontology.parser;
+package ai.hyperlearning.ontopop.data.ontology.parser.function;
+
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import ai.hyperlearning.ontopop.messaging.processors.DataPipelineParserSource;
+import ai.hyperlearning.ontopop.data.ontology.parser.OntologyParserService;
 import ai.hyperlearning.ontopop.model.ontology.OntologyMessage;
 
 /**
- * Ontology Parsing Service - Spring Boot Application
+ * Ontology Parser Function
  *
  * @author jillurquddus
  * @since 2.0.0
  */
 
-@SuppressWarnings("deprecation")
-@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
-@SpringBootApplication
-@EnableBinding(DataPipelineParserSource.class)
-public class OntologyParserApp {
-
+@Component
+public class OntologyParserFunction 
+    implements Function<OntologyParserFunctionModel, Boolean> {
+    
     private static final Logger LOGGER =
-            LoggerFactory.getLogger(OntologyParserApp.class);
-
+            LoggerFactory.getLogger(OntologyParserFunction.class);
+    
     @Autowired
     private OntologyParserService ontologyParserService;
-
-    public static void main(String[] args) {
-        SpringApplication.run(OntologyParserApp.class, args);
-    }
-
-    @StreamListener("validatedConsumptionChannel")
-    public void processValidatedOntology(String payload) {
-
+    
+    @Override
+    public Boolean apply(
+            OntologyParserFunctionModel ontologyParserFunctionModel) {
+        
         try {
 
             // Explicitly check that the string payload
             // models an OntologyMessage object
             ObjectMapper mapper = new ObjectMapper();
             OntologyMessage ontologyMessage =
-                    mapper.readValue(payload, OntologyMessage.class);
+                    mapper.readValue(
+                            ontologyParserFunctionModel.getPayload(), 
+                            OntologyMessage.class);
 
             // Log the consumed payload for debugging purposes
             LOGGER.debug("New ontology validation event detected and consumed "
                     + "via the shared messaging service and the "
                     + "validatedConsumptionChannel channel.");
-            LOGGER.debug("Ontology validation message payload: {}", payload);
+            LOGGER.debug("Ontology validation message payload: {}", 
+                    ontologyParserFunctionModel.getPayload());
 
             // Run the Ontology Parsing Service pipeline
             ontologyParserService.run(ontologyMessage);
@@ -66,7 +62,9 @@ public class OntologyParserApp {
             LOGGER.info("The validated object is NOT an ontology. Skipping.");
 
         }
-
+        
+        return true;
+        
     }
 
 }
