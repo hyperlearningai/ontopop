63d471e3bcb55e280a71170b42217b75c9e28412
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-17 10:17:52 +0000

    Add triplestore loader Azure function

---
+++ ontopop-apps-azure/ontopop-azure-function-app-data-ontology-loader-triplestore/src/main/java/ai/hyperlearning/ontopop/apps/azure/functions/data/ontology/loader/triplestore/OntologyTriplestoreLoaderAzureFunctionHandler.java
@@ -0,0 +1,49 @@
+package ai.hyperlearning.ontopop.apps.azure.functions.data.ontology.loader.triplestore;
+
+import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.AccessRights;
+import com.microsoft.azure.functions.annotation.Cardinality;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.ServiceBusTopicTrigger;
+
+import ai.hyperlearning.ontopop.data.ontology.loader.triplestore.function.OntologyTriplestoreLoaderFunctionModel;
+
+/**
+ * Ontology Triplestore Loader Function - Azure Handler
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+public class OntologyTriplestoreLoaderAzureFunctionHandler 
+        extends FunctionInvoker<OntologyTriplestoreLoaderFunctionModel, Boolean> {
+    
+    @FunctionName("ontologyTriplestoreLoaderFunction")
+    public void run(
+            @ServiceBusTopicTrigger(
+                    name = "message",
+                    topicName = "%TOPIC_NAME%",
+                    subscriptionName = "%SUBSCRIPTION_NAME%",
+                    connection = "AZURE_SERVICEBUS_CONNECTION_STRING", 
+                    access = AccessRights.LISTEN, 
+                    dataType = "string", 
+                    cardinality = Cardinality.ONE, 
+                    isSessionsEnabled = false) 
+                String message,
+            final ExecutionContext context) {
+        
+        // Log the service bus trigger and message for debugging purposes
+        context.getLogger().info("Ontology Triplestore Loader Function triggered: "
+                + "New service bus event - Ontology validation event.");
+        context.getLogger().info("Service bus message payload: " + message);
+        
+        // Execute the Ontology Triplestore Loader function
+        OntologyTriplestoreLoaderFunctionModel ontologyTriplestoreLoaderFunctionModel = 
+                new OntologyTriplestoreLoaderFunctionModel(message);
+        handleRequest(ontologyTriplestoreLoaderFunctionModel, context);
+        
+    }
+
+}
