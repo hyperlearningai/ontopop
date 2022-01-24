e8b7fa74f05a8c786d9c1752ae589df6464b4661
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-24 09:12:13 +0000

    Add support for Fuseki basic authentication

--- ontopop-triplestore/src/main/java/ai/hyperlearning/ontopop/triplestore/jena/JenaFusekiWebClientConfig.java
+++ ontopop-triplestore/src/main/java/ai/hyperlearning/ontopop/triplestore/jena/JenaFusekiWebClientConfig.java
@@ -1,5 +1,6 @@
 package ai.hyperlearning.ontopop.triplestore.jena;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -22,20 +23,49 @@ public class JenaFusekiWebClientConfig {
 
     @Value("${storage.triplestore.apache-jena.fuseki.url}")
     private String fusekiUrl;
+    
+    @Value("${storage.triplestore.apache-jena.fuseki.username}")
+    private String fusekiUsername;
+    
+    @Value("${storage.triplestore.apache-jena.fuseki.password}")
+    private String fusekiPassword;
 
     @Value("${web.client.codecs.maxInMemorySize}")
     private int webClientMaxInMemorySize;
 
     @Bean("fusekiWebClient")
-    public WebClient fusekiWebClient() {
+    public WebClient getFusekiWebClient() {
 
-        return WebClient.builder()
-                .exchangeStrategies(ExchangeStrategies.builder()
-                        .codecs(configurer -> configurer.defaultCodecs()
-                                .maxInMemorySize(
-                                        webClientMaxInMemorySize * 1024 * 1024))
-                        .build())
-                .baseUrl(fusekiUrl).build();
+        if ( StringUtils.isBlank(fusekiUsername) ) {
+            
+            return WebClient.builder()
+                    .exchangeStrategies(ExchangeStrategies.builder()
+                            .codecs(configurer -> configurer.defaultCodecs()
+                                    .maxInMemorySize(
+                                            webClientMaxInMemorySize * 1024 * 1024))
+                            .build())
+                    .baseUrl(fusekiUrl)
+                    .build();
+            
+        } else {
+            
+            // HTTP Basic Authentication
+            // Make sure that the relevant username and password has been
+            // defined in ${FUSEKI-SERVER}/run/shiro.ini
+            // Reference: https://jena.apache.org/documentation/fuseki2/fuseki-security.html
+            return WebClient.builder()
+                    .exchangeStrategies(ExchangeStrategies.builder()
+                            .codecs(configurer -> configurer.defaultCodecs()
+                                    .maxInMemorySize(
+                                            webClientMaxInMemorySize * 1024 * 1024))
+                            .build())
+                    .baseUrl(fusekiUrl)
+                    .defaultHeaders(
+                            header -> header.setBasicAuth(
+                                    fusekiUsername, fusekiPassword))
+                    .build();
+            
+        }
 
     }
 
