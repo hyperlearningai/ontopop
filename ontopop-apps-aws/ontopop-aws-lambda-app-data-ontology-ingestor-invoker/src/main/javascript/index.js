8085126e81fec2fd9e6cc27fbfc90b834ca4ea09
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-25 11:56:38 +0000

    Add further comments

--- ontopop-apps-aws/ontopop-aws-lambda-app-data-ontology-ingestor-invoker/src/main/javascript/index.js
+++ ontopop-apps-aws/ontopop-aws-lambda-app-data-ontology-ingestor-invoker/src/main/javascript/index.js
@@ -1,19 +1,23 @@
 const AWS = require('aws-sdk');
 exports.handler = async (event) => {
     
-    // Define the ontology ingestor function name as an environmental variable
+    // Define the ontology ingestor function name as an environmental variable.
+    // Note that this lambda (invoker) will require permissions to invoke
+    // the lambda (invokee) below via the lambda:InvokeFunction action.
+    // Reference: https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html
     let functionName = process.env.FUNCTION_NAME_ONTOLOGY_INGESTOR;
     
-    // InvocationType of Event invokes the function asynchronously
+    // InvocationType of Event invokes the function asynchronously.
     const params = {
         FunctionName: functionName,
         InvocationType: 'Event',
         Payload: JSON.stringify(event)
     };
     
-    // Invoke the lambda but do NOT wait for a response
-    // This is necessary as GitHub Webhook requests timeout after 10 seconds
-    // so an immediate response is required
+    // Invoke the lambda but do NOT wait for a response.
+    // This is necessary as GitHub Webhook requests timeout after 10 seconds, 
+    // after which the HTTP connection is destroyed and the webhook payload 
+    // lost, so an immediate response is required.
     let lambda = new AWS.Lambda();
     await lambda.invoke(params).promise();
     
