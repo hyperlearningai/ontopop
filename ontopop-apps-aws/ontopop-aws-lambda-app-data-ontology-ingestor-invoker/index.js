bd74458354833a0eb1d0971a8d68f4255ffd9fa5
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-01-27 12:47:36 +0000

    Add ontology ingestion invoker Node.js lambda app

---
+++ ontopop-apps-aws/ontopop-aws-lambda-app-data-ontology-ingestor-invoker/index.js
@@ -0,0 +1,40 @@
+/**
+ * Ontology Ingestion Service Invoker
+ *
+ * An API Gateway HTTP-triggered (POST) AWS lambda application written in 
+ * Node.js that will be requested by the Git webhook (POST). This AWS
+ * lambda function simply sends the API Gateway Proxy Request Event object
+ * to the Ontology Ingestion Service (which is another AWS lambda application). 
+ * It invokes the Ontology Ingestion Service AWS lambda application, but it
+ * does NOT wait for a response - this is necessary as GitHub Webhook requests 
+ * timeout after 10 seconds, after which the HTTP connection is destroyed and 
+ * the webhook payload lost, so an immediate response is required.
+ *
+ * @author jillurquddus
+ * @since  2.0.0
+ */
+
+const AWS = require('aws-sdk');
+exports.handler = async (event) => {
+    
+    // Define the ontology ingestor function name as an environmental variable.
+    // Note that this lambda (invoker) will require permissions to invoke
+    // the lambda (invokee) below via the lambda:InvokeFunction action.
+    // Reference: https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html
+    let functionName = process.env.FUNCTION_NAME_ONTOLOGY_INGESTOR;
+    
+    // InvocationType of Event invokes the function asynchronously.
+    const params = {
+        FunctionName: functionName,
+        InvocationType: 'Event',
+        Payload: JSON.stringify(event)
+    };
+    
+    // Invoke the lambda but do NOT wait for a response.
+    // This is necessary as GitHub Webhook requests timeout after 10 seconds, 
+    // after which the HTTP connection is destroyed and the webhook payload 
+    // lost, so an immediate response is required.
+    let lambda = new AWS.Lambda();
+    await lambda.invoke(params).promise();
+    
+};
