2f6989e4ea439b9dd72d70bc1262c5cb80e48805
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-02-14 10:17:41 +0000

    Add WebProtege exporter Spring Boot app

---
+++ ontopop-apps/ontopop-app-data-ontology-webprotege-exporter/src/main/java/ai/hyperlearning/ontopop/apps/data/ontology/webprotege/exporter/WebProtegeExporterApp.java
@@ -0,0 +1,41 @@
+package ai.hyperlearning.ontopop.apps.data.ontology.webprotege.exporter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.ComponentScan;
+
+import ai.hyperlearning.ontopop.data.ontology.webprotege.exporter.function.WebProtegeExporterFunction;
+import ai.hyperlearning.ontopop.messaging.processors.DataPipelineWebProtegeExporterSource;
+
+/**
+ * WebProtege Exporter Service - Spring Boot Application
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@SuppressWarnings("deprecation")
+@ComponentScan(basePackages = {"ai.hyperlearning.ontopop"})
+@SpringBootApplication
+@EnableBinding(DataPipelineWebProtegeExporterSource.class)
+public class WebProtegeExporterApp {
+    
+    @Autowired
+    private WebProtegeExporterFunction webProtegeExporterFunction;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WebProtegeExporterFunction.class, args);
+    }
+    
+    @StreamListener("webProtegeProjectUpdatedConsumptionChannel")
+    public void processIngestedOntology(String payload) {
+
+        // Execute the WebProtege Exporter Function
+        webProtegeExporterFunction.accept(payload);
+
+    }
+
+}
