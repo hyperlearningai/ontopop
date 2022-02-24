b49d9fbd6e5b3130d4e19e1b1a3d563c1f6a85ea
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-02-24 20:40:51 +0000

    Change search endpoint to POST mapping

--- ontopop-api-ontology-search/src/main/java/ai/hyperlearning/ontopop/api/ontology/search/OntologySearchController.java
+++ ontopop-api-ontology-search/src/main/java/ai/hyperlearning/ontopop/api/ontology/search/OntologySearchController.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -69,7 +69,7 @@ public class OntologySearchController {
     }
     
     /**************************************************************************
-     * GET Search Query
+     * POST Search Query
      *************************************************************************/
     
     @Operation(
@@ -89,7 +89,7 @@ public class OntologySearchController {
                             responseCode = "500",
                             description = "Internal server error.")})
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(
+    @PostMapping(
             value = "/{id}", 
             consumes = MediaType.APPLICATION_JSON_VALUE, 
             produces = MediaType.APPLICATION_JSON_VALUE)
