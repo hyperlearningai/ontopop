55bd2e574f8628701d18d8ded22258e032e2c4ab
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-02-25 13:45:45 +0000

    Cast event as string

--- ontopop-apps-aws/ontopop-aws-lambda-app-data-ontology-webprotege-exporter-scheduler/src/main/java/ai/hyperlearning/ontopop/apps/aws/lambda/data/ontology/webprotege/exporter/WebProtegeExporterSchedulerAwsLambdaEventBridgeConsumer.java
+++ ontopop-apps-aws/ontopop-aws-lambda-app-data-ontology-webprotege-exporter-scheduler/src/main/java/ai/hyperlearning/ontopop/apps/aws/lambda/data/ontology/webprotege/exporter/WebProtegeExporterSchedulerAwsLambdaEventBridgeConsumer.java
@@ -1,6 +1,5 @@
 package ai.hyperlearning.ontopop.apps.aws.lambda.data.ontology.webprotege.exporter;
 
-import java.util.Map;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Component;
 import ai.hyperlearning.ontopop.data.ontology.webprotege.exporter.scheduler.WebProtegeExporterSchedulerService;
 
 /**
- * AWS WebProtege Exporter Scheduler
+ * AWS WebProtege Exporter Scheduler Event Bridge Consumer
  * The event emitted from AWS Event Bridge is consumed as a Map
  *
  * @author jillurquddus
@@ -19,17 +18,17 @@ import ai.hyperlearning.ontopop.data.ontology.webprotege.exporter.scheduler.WebP
  */
 
 @Component
-public class WebProtegeExporterScheduler 
-        implements Consumer<Map<String,String>> {
+public class WebProtegeExporterSchedulerAwsLambdaEventBridgeConsumer 
+        implements Consumer<String> {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(
-            WebProtegeExporterScheduler.class);
+            WebProtegeExporterSchedulerAwsLambdaEventBridgeConsumer.class);
     
     @Autowired
     private WebProtegeExporterSchedulerService webProtegeExporterSchedulerService;
 
     @Override
-    public void accept(Map<String, String> event) {
+    public void accept(String event) {
         LOGGER.debug("WebProtege Exporter Scheduler Service event: {}", event);
         LOGGER.info("Running the WebProtege Exporter Scheduler Service.");
         webProtegeExporterSchedulerService.runScheduledWebProtegeExporterService();
