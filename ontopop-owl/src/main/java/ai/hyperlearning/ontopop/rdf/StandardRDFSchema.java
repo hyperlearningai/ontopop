4ada946a2c6a1c19bbe86f8ed561162c8319894e
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-03-23 12:22:13 +0000

    Add RDF standard schema loader

---
+++ ontopop-owl/src/main/java/ai/hyperlearning/ontopop/rdf/StandardRDFSchema.java
@@ -0,0 +1,79 @@
+package ai.hyperlearning.ontopop.rdf;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+import ai.hyperlearning.ontopop.model.owl.SimpleAnnotationProperty;
+
+/**
+ * Standard RDF Schema
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+public class StandardRDFSchema {
+    
+    /**
+     * Load and parse the standard SKOS, RDF and DCMI RDF schema
+     * @return
+     * @throws OWLOntologyCreationException
+     * @throws IOException
+     */
+    
+    public static Map<String, SimpleAnnotationProperty> loadStandardSchema() 
+            throws OWLOntologyCreationException, IOException {
+        
+        // Load the SKOS Vocabulary and parse its annotation properties
+        OWLOntology skos = SKOSVocabulary.loadSKOSRDF();
+        Map<String, SimpleAnnotationProperty> skosAnnotationProperties =
+                SKOSVocabulary.parseAnnotationProperties(skos);
+
+        // Load the RDF Schema and parse its annotation properties
+        OWLOntology rdf = RDFSchema.loadRdfSchema();
+        Map<String, SimpleAnnotationProperty> rdfSchemaAnnotationProperties =
+                RDFSchema.parseAnnotationProperties(rdf);
+
+        // Load the DCMI RDF Schema and parse its annotation properties
+        Map<String, SimpleAnnotationProperty> dcmiSchemaAnnotationProperties =
+                DCMI.parseAnnotationProperties();
+
+        // Aggregate the standard schema annotation properties
+        Map<String, SimpleAnnotationProperty> standardSchemaAnnotationProperties =
+                new LinkedHashMap<>(skosAnnotationProperties);
+        standardSchemaAnnotationProperties
+                .putAll(rdfSchemaAnnotationProperties);
+        standardSchemaAnnotationProperties
+                .putAll(dcmiSchemaAnnotationProperties);
+        return standardSchemaAnnotationProperties;
+        
+    }
+    
+    /**
+     * Generate a unique set of standard schema annotation property labels
+     * @return
+     * @throws OWLOntologyCreationException
+     * @throws IOException
+     */
+    
+    public static Set<String> getUniqueStandardSchemaAnnotationPropertyLabels() 
+            throws OWLOntologyCreationException, IOException {
+        Map<String, SimpleAnnotationProperty> standardSchema = loadStandardSchema();
+        Set<String> standardSchemaAnnotationPropertyLabels = new HashSet<>();
+        for ( SimpleAnnotationProperty simpleAnnotationProperty : 
+            standardSchema.values() ) {
+            if ( simpleAnnotationProperty.getLabel() != null )
+                standardSchemaAnnotationPropertyLabels.add(
+                        simpleAnnotationProperty.getLabel()
+                            .strip().toUpperCase());
+        }
+        return standardSchemaAnnotationPropertyLabels;
+    }
+
+}
