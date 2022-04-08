339c02dab417830f8ac907647359690f907a558c
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-04-08 09:15:30 +0100

    Add Mapping API Azure Webapp

---
+++ ontopop-apps/ontopop-apps-azure/ontopop-azure-app-api-ontology-mapping/src/main/java/ai/hyperlearning/ontopop/apps/azure/api/ontology/mapping/OntologyMappingApiAzureAppServerFactoryConfig.java
@@ -0,0 +1,27 @@
+package ai.hyperlearning.ontopop.apps.azure.api.ontology.mapping;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+/**
+ * Microsoft Ontology Mapping API Service Web Application - Server Factory Configuration
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@Component
+public class OntologyMappingApiAzureAppServerFactoryConfig 
+        implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    
+    @Value("${web.azure.webapps.port:80}")
+    private int port;
+    
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(port);
+    }
+
+}
