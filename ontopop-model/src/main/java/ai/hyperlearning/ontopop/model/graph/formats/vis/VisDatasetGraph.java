bcefdcae5735f64e8bd3fa9dc63a5b57a9bbcd53
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-05-06 12:25:19 +0100

    Add Vis.js property graph model

--- ontopop-model/src/main/java/ai/hyperlearning/ontopop/model/graph/formats/vis/VisDatasetGraph.java
+++ ontopop-model/src/main/java/ai/hyperlearning/ontopop/model/graph/formats/vis/VisDatasetGraph.java
@@ -1,4 +1,4 @@
-package ai.hyperlearning.ontopop.model.graph.formats.graphson;
+package ai.hyperlearning.ontopop.model.graph.formats.vis;
 
 import java.io.Serializable;
 import java.util.LinkedHashMap;
@@ -8,53 +8,38 @@ import java.util.Map;
 import ai.hyperlearning.ontopop.model.graph.formats.PropertyGraphFormat;
 
 /**
- * GraphSON Graph Model
+ * Vis.js Dataset Graph Model
  *
  * @author jillurquddus
  * @since 2.0.0
  */
 
-public class GraphSONGraph extends PropertyGraphFormat implements Serializable  {
+public class VisDatasetGraph extends PropertyGraphFormat implements Serializable {
 
-    private static final long serialVersionUID = 799775699830477685L;
+    private static final long serialVersionUID = -4080082062382561699L;
     
     // Internal property keys
-    private static final String COMMON_PROPERTY_KEY_ID = "_id";
-    private static final String COMMON_PROPERTY_KEY_TYPE = "_type";
-    private static final String COMMON_PROPERTY_KEY_LABEL = "_label";
-    private static final String VERTEX_PROPERTY_KEY_RDFS_LABEL = "label";
-    private static final String VERTEX_PROPERTY_KEY_NAME = "name";
-    private static final String EDGE_PROPERTY_KEY_OUT_V = "_outV";
-    private static final String EDGE_PROPERTY_KEY_IN_V = "_inV";
+    private static final String COMMON_PROPERTY_KEY_ID = "id";
+    private static final String COMMON_PROPERTY_KEY_LABEL = "type";
+    private static final String EDGE_PROPERTY_KEY_OUT_V = "from";
+    private static final String EDGE_PROPERTY_KEY_IN_V = "to";
     private static final String EDGE_PROPERTY_KEY_RELATIONSHIP = "relationship";
     private static final String EDGE_PROPERTY_KEY_WEIGHT = "weight";
     
     // Internal property key values
-    private static final String VERTEX_PROPERTY_VALUE_TYPE = "vertex";
     private static final String VERTEX_PROPERTY_VALUE_LABEL = "class";
-    private static final String EDGE_PROPERTY_VALUE_TYPE = "edge";
     private static final String EDGE_PROPERTY_VALUE_LABEL = "subclassOf";
     private static final String EDGE_PROPERTY_VALUE_RELATIONSHIP = "Subclass of";
     private static final int EDGE_PROPERTY_VALUE_WEIGHT = 1;
     
-    private String mode = "NORMAL";
-    
-    public GraphSONGraph() {
+    public VisDatasetGraph() {
         
     }
 
-    public GraphSONGraph(List<Map<String, Object>> vertices,
+    public VisDatasetGraph(List<Map<String, Object>> vertices, 
             List<Map<String, Object>> edges) {
         this.vertices = vertices;
         this.edges = edges;
-    }
-
-    public String getMode() {
-        return mode;
-    }
-
-    public void setMode(String mode) {
-        this.mode = mode;
     }
     
     public void addVertex(long vertexId, Map<String, Object> properties) {
@@ -62,17 +47,8 @@ public class GraphSONGraph extends PropertyGraphFormat implements Serializable  
         // Add vertex properties
         Map<String, Object> propertyMap = new LinkedHashMap<>();
         propertyMap.put(COMMON_PROPERTY_KEY_ID, String.valueOf(vertexId));
-        propertyMap.put(COMMON_PROPERTY_KEY_TYPE, VERTEX_PROPERTY_VALUE_TYPE);
         propertyMap.put(COMMON_PROPERTY_KEY_LABEL, VERTEX_PROPERTY_VALUE_LABEL);
         propertyMap.putAll(properties);
-        
-        // Explicitly generate the name property
-        if ( propertyMap.containsKey(VERTEX_PROPERTY_KEY_RDFS_LABEL) ) {
-            String rdfsLabel = (String) propertyMap.get(
-                    VERTEX_PROPERTY_KEY_RDFS_LABEL);
-            propertyMap.put(VERTEX_PROPERTY_KEY_NAME, rdfsLabel);
-            propertyMap.remove(VERTEX_PROPERTY_KEY_RDFS_LABEL);
-        }
         
         // Add the vertex to the list of vertices
         this.vertices.add(propertyMap);
@@ -85,7 +61,6 @@ public class GraphSONGraph extends PropertyGraphFormat implements Serializable  
         // Add edge properties
         Map<String, Object> propertyMap = new LinkedHashMap<>();
         propertyMap.put(COMMON_PROPERTY_KEY_ID, String.valueOf(edgeId));
-        propertyMap.put(COMMON_PROPERTY_KEY_TYPE, EDGE_PROPERTY_VALUE_TYPE);
         propertyMap.put(COMMON_PROPERTY_KEY_LABEL, EDGE_PROPERTY_VALUE_LABEL);
         propertyMap.put(EDGE_PROPERTY_KEY_OUT_V, String.valueOf(sourceVertexId));
         propertyMap.put(EDGE_PROPERTY_KEY_IN_V, String.valueOf(targetVertexId));
@@ -104,8 +79,7 @@ public class GraphSONGraph extends PropertyGraphFormat implements Serializable  
 
     @Override
     public String toString() {
-        return "GraphSONGraph ["
-                + "mode=" + mode + ", "
+        return "VisDatasetGraph ["
                 + "vertices=" + this.vertices + ", "
                 + "edges=" + this.edges 
                 + "]";
