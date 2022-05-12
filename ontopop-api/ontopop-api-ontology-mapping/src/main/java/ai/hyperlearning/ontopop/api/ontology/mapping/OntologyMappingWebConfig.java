7da30151813ce5b6a20a84c2ec04859aaee1fe5a
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-05-12 20:59:21 +0100

    Add CORS mapping

---
+++ ontopop-api/ontopop-api-ontology-mapping/src/main/java/ai/hyperlearning/ontopop/api/ontology/mapping/OntologyMappingWebConfig.java
@@ -0,0 +1,24 @@
+package ai.hyperlearning.ontopop.api.ontology.mapping;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Ontology Mapping API Service - Web Configuration
+ *
+ * @author jillurquddus
+ * @since 2.0.0
+ */
+
+@Configuration
+@EnableWebMvc
+public class OntologyMappingWebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
