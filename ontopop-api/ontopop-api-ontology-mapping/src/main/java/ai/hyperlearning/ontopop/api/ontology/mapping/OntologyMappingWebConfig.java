a0b843fec1e49a4a205989644411f024da241286
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-05-12 21:35:33 +0100

    Configure CORS allowed origins

--- ontopop-api/ontopop-api-ontology-mapping/src/main/java/ai/hyperlearning/ontopop/api/ontology/mapping/OntologyMappingWebConfig.java
+++ ontopop-api/ontopop-api-ontology-mapping/src/main/java/ai/hyperlearning/ontopop/api/ontology/mapping/OntologyMappingWebConfig.java
@@ -18,7 +18,9 @@ public class OntologyMappingWebConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*");
     }
 
 }
