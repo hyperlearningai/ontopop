65cf2d0f93b36c8e75e73e6902f23277721e8604
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-05-17 15:15:26 +0100

    Add task scheduler thread pool

--- ontopop-core/ontopop-webprotege/src/main/java/ai/hyperlearning/ontopop/webprotege/WebProtegeAuthSchedulerConfig.java
+++ ontopop-core/ontopop-webprotege/src/main/java/ai/hyperlearning/ontopop/webprotege/WebProtegeAuthSchedulerConfig.java
@@ -1,8 +1,11 @@
 package ai.hyperlearning.ontopop.webprotege;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * WebProtege Authentication Scheduler Configuration
@@ -17,6 +20,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         + "'${plugins.webprotege.authenticator.scheduler.enabled}'.equals('true')")
 public class WebProtegeAuthSchedulerConfig {
     
+    private static final int TASK_SCHEDULER_POOL_SIZE = 2;
+    private static final String TASK_SCHEDULER_THREAD_NAME_PREFIX = 
+            "OntoPopThreadPoolTaskScheduler";
     
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = 
+                new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(
+                TASK_SCHEDULER_POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix(
+                TASK_SCHEDULER_THREAD_NAME_PREFIX);
+        return threadPoolTaskScheduler;
+    }
 
 }
