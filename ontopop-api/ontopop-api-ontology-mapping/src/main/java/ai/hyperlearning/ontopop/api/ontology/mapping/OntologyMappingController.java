bbbe04ef43745e77c43dfe7d6ebc1e48d4014be5
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-05-20 10:49:36 +0100

    Add RestController annotation

--- ontopop-api/ontopop-api-ontology-mapping/src/main/java/ai/hyperlearning/ontopop/api/ontology/mapping/OntologyMappingController.java
+++ ontopop-api/ontopop-api-ontology-mapping/src/main/java/ai/hyperlearning/ontopop/api/ontology/mapping/OntologyMappingController.java
@@ -13,11 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import ai.hyperlearning.ontopop.exceptions.ontology.OntologyDataParsingException;
@@ -45,7 +45,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
  * @since 2.0.0
  */
 
-@Controller
+@RestController
 @RequestMapping("/mapping")
 @Tag(name = "Mapping API", description = "API for undertaking common mapping operations")
 public class OntologyMappingController {
