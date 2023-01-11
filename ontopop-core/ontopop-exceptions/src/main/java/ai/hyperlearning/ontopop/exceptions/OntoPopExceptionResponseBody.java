d7e5593992c110b092e33e3763a0f9c7e3dfdb8d
jillurquddus <jillur.quddus@hyperlearning.ai>
2023-01-11 19:35:07 +0000

    Add error code

--- ontopop-core/ontopop-exceptions/src/main/java/ai/hyperlearning/ontopop/exceptions/OntoPopExceptionResponseBody.java
+++ ontopop-core/ontopop-exceptions/src/main/java/ai/hyperlearning/ontopop/exceptions/OntoPopExceptionResponseBody.java
@@ -12,29 +12,29 @@ import java.io.Serializable;
 public class OntoPopExceptionResponseBody implements Serializable {
 
     private static final long serialVersionUID = -6791529013463816400L;
-    private String errorKey;
     private int errorCode;
+    private String errorKey;
     private String errorMessage;
     
     public OntoPopExceptionResponseBody(String errorKey, 
             String errorMessage) {
-        this.errorKey = errorKey;
         int pipeIndex = errorMessage.indexOf('|');
         this.errorCode = (pipeIndex > -1) ?
                 Integer.parseInt(errorMessage.substring(0, pipeIndex))
                     : 3001;
+        this.errorKey = errorKey;
         this.errorMessage = (pipeIndex > -1) ?
                 errorMessage.substring(pipeIndex + 1) : errorMessage ;
+    }
+    
+    public int getErrorCode() {
+        return errorCode;
     }
     
     public String getErrorKey() {
         return errorKey;
     }
     
-    public int getErrorCode() {
-        return errorCode;
-    }
-
     public String getErrorMessage() {
         return errorMessage;
     }
@@ -42,8 +42,8 @@ public class OntoPopExceptionResponseBody implements Serializable {
     @Override
     public String toString() {
         return "OntoPopExceptionResponseBody ["
-                + "errorKey=" + errorKey + ", "
                 + "errorCode=" + errorCode + ", "
+                + "errorKey=" + errorKey + ", "
                 + "errorMessage=" + errorMessage 
                 + "]";
     }
